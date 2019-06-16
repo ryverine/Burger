f7b3dfc4dc92933597bb9497f034e0e9607febae
ryverine <rwcheek@gmail.com>
2019-06-16 11:00:36 -0400

    Database schema and seeds completed.

---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+	INSERT INTO burgers (burger_name, devoured)
+	VALUES 
+		("Big Kahuna Burger", false),
+		("BFloppy Burger", false),
+		("BKrabby Patty", false),
+		("BMother Nature Burger", false),
+		("BSloppy Burger", false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
