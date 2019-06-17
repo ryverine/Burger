fdc70235822a997696008f616179c1dd3760af63
ryverine <rwcheek@gmail.com>
2019-06-17 12:58:44 -0400

    Content is going to index.handlebars, but it is only visible after I kill the app in the terminal. I think this must be due to the callback functions.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,26 +1,31 @@
 
 var express = require("express");
+var exphbs = require("express-handlebars");
 
 var burger = require("../models/burger.js");
-
-
 
 module.exports = function(app) 
 {
     // home
     app.get("/api/all", function(req, res) 
     {
-        // res.render("dog", animals[0]);
         console.log("/api/all");
-        //  getBurgers: function()
-        
-        // res.json(burger.getBurgers());
-        // console.log();
-
-        var data = burger.getBurgers(function(result)
+      
+        burger.getBurgers(function(result)
         {
             console.log("burgers_controller result", result);
+            // data = result;
+            res.render("index", { burgers: result });
         });
+
+        //console.log("burgers_controller -> /api/all -> data", data);
+
+        /*
+        app.get("/icecreams", function(req, res) 
+        {
+            res.render("ics", { ics: icecreams });
+        });
+        */
         
         // console.log("res = ", res);
 
@@ -30,7 +35,7 @@ module.exports = function(app)
     {
         //res.render("dog", animals[0]);
         console.log("/api/add");
-          // addBurger: function(burgerName)
+        // addBurger: function(burgerName)
     });
 
     app.put("/api/:burger", function(req, res) 
--- server.js
+++ server.js
@@ -7,11 +7,11 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 require("./controllers/burgers_controller.js")(app);
 
